a11d729f88392484c21cfae3db91593e7726ac0a
Kenny Alexander Berrones Berrones <kabb1@clL04A-14>
2021-12-02 14:33:24 +0100

    Adelantos

--- Practicas/Practica 2/main.py
+++ Practicas/Practica 2/main.py
@@ -1,9 +1,10 @@
 # Importamos las librerias que necesitaremos
 import numpy as np
 import matplotlib.pyplot as plt
-import utils
-import adaboost
+
 import clasificador_debil as cd
+import adaboost
+import utils
 
 # Cargamos la base de datos
 npzfile = np.load("mnist.npz")
@@ -15,7 +16,7 @@ mnist_Y = npzfile['y']
 (X, Y) = utils.adaptar_conjuntos(mnist_X, mnist_Y)
 
 # Lanzar Adaboost
-T = 20
+T = 15
 A = 50
 
 
@@ -28,37 +29,50 @@ N = len(pruebaX)
 (Hx, alphas) = adaboost.entrenar(pruebaX, pruebaY, T, A)
 
 
-#resultado = np.ones(N)
-#posiciones = np.zeros(N)
+resultado = np.ones(N)
+posiciones = np.zeros(N)
+aciertos = 0
+fallos = 0
 
 #Para ver si va
-#for idx in range(0, N):
-#    sumatorio = 0
-#    for deb in Hx:
-#        sumatorio = sumatorio + deb.confianza * 1 if cd.aplicar_clasificador_debil(deb, X[idx]) else -1
-#    if sumatorio > 0:
-#        resultado[idx] =  1
-#    else:
-#        resultado[idx] = -1
-#    posiciones[idx] = idx
+for idx in range(0, N):
+    sumatorio = 0
+    for deb in Hx:
+        sumatorio = sumatorio + deb.confianza * 1 if cd.aplicar_clasificador_debil(deb, X[idx]) else -1
+    if sumatorio > 0:
+        resultado[idx] =  1
+    else:
+        resultado[idx] = -1
+    posiciones[idx] = idx
 
+for i in resultado:
+    if i == 1:
+        aciertos += 1
+    else:
+        fallos += 1
 
-for i in range(N):
-    if pruebaY[i]:
-        print("Pos :", i)
+tasa_aciertos = aciertos / N
+tasa_fallos = fallos / N
 
-sumatorio = 0
+print("La tasa de aciertos es: ", tasa_aciertos, " %")
+print("La tasa de fallos es: ", tasa_fallos, " %")
 
-for deb in Hx:
-    sumatorio = sumatorio + deb.confianza * 1 if cd.aplicar_clasificador_debil(deb, pruebaX[1]) else -1
+#for i in range(N):
+#    if pruebaY[i]:
+#        print("Pos :", i)
 
-print("Valor: ", sumatorio)
-if sumatorio > 0:
-    print("La imagen es un 0")
-else:
-    print("La imagen no es un 0")
+#sumatorio = 0
 
-utils.mostrar_imagen(mnist_X[1])
+#for deb in Hx:
+#    sumatorio = sumatorio + deb.confianza * 1 if cd.aplicar_clasificador_debil(deb, pruebaX[1]) else -1
+
+#print("Valor: ", sumatorio)
+#if sumatorio > 0:
+#    print("La imagen es un 0")
+#else:
+#    print("La imagen no es un 0")
+
+#utils.mostrar_imagen(pruebaX[1].reshape(28, 28))
 
 total = np.count_nonzero(resultado == 1)
 print("Total ceros con el algoritmo en los ", N, " primeros numeros: ", total)
